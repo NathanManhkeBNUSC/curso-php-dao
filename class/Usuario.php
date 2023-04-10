383d49530c0aa061d2bbf35401a4333e70872c16
NathanManhkeBNUSC <nathanmanhke@gmail.com>
2023-04-10 16:01:41 -0300

    Criando a classe Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,3 +1,75 @@
 <?php
 
+class Usuario{
+
+    private $id;
+    private $nome;
+    private $usuario;
+    private $senha;
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($value){
+        $this->id = $value;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setNome($value){
+        $this->nome = $value;
+    }
+
+    public function getUsuario(){
+        return $this->usuario;
+    }
+
+    public function setUsuario($value){
+        $this->usuario = $value;
+    }
+
+    public function getSenha(){
+        return $this->senha;
+    }
+
+    public function setSenha($value){
+        $this->senha = $value;
+    }
+
+    public function loadById($id){
+
+        $sql = new Sql();
+
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE id = :id", array(
+            ":id"=>$id
+        ));
+
+        //Isset significa para verificar se algo exite
+        if (isset($result[0])) {
+
+            $row = $result[0];
+
+            $this->setId($row['id']);
+            $this->setNome($row['nome']);
+            $this->setUsuario($row['usuario']);
+            $this->setSenha($row['senha']);
+
+        }
+
+    }
+
+    public function __toString(){
+        return json_encode(array(
+            "id"=>$this->getId(),
+            "nome"=>$this->getNome(),
+            "usuario"=>$this->getUsuario(),
+            "senha"=>$this->getSenha()
+        ));
+    }
+
+}
+
 ?>
